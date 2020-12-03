0b13673beaca42c06d727de648cf90dbac8a9ef8
ozgurgelekci <ozgurgelekci@aof.anadolu.edu.tr>
2020-12-03 17:11:39 +0300

    create projects and entities

---
+++ NetCoreApiProject.Core/Entities/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+using System.Reflection.PortableExecutable;
+using System.Text;
+
+namespace NetCoreApiProject.Core.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int Stock { get; set; }
+        public int CategoryId { get; set; }
+        public bool IsDeleted { get; set; }
+        public string InnerBarcode { get; set; }
+
+        public virtual Category Category { get; set; }
+    }
+}

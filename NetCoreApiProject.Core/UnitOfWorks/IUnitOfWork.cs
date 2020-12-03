75642ca32714f5a666557a7064d34d2f53642fee
ozgurgelekci <ozgurgelekci@aof.anadolu.edu.tr>
2020-12-03 18:11:32 +0300

    unit of work interface added

---
+++ NetCoreApiProject.Core/UnitOfWorks/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using NetCoreApiProject.Core.Repositories;
+
+namespace NetCoreApiProject.Core.UnitOfWorks
+{
+    public interface IUnitOfWork
+    {
+        IProductRepository Product { get; }
+        ICategoryRepository Category { get; }
+
+        Task CommitAsync();
+        void Commit();
+    }
+}

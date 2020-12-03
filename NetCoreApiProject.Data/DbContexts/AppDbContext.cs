f6f0e29f5ca8f01003676b3c380eeccf1d550e3c
ozgurgelekci <ozgurgelekci@aof.anadolu.edu.tr>
2020-12-03 18:19:45 +0300

    DbContext added

---
+++ NetCoreApiProject.Data/DbContexts/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NetCoreApiProject.Core.Entities.Concrete;
+
+namespace NetCoreApiProject.Data.DbContexts
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+
+        private DbSet<Category> Categories { get; set; }
+
+        private DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
